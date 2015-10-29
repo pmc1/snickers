9d03e7a167000f3528b70e39a42789e7d31bed80
paul <paulmchoI@gmail.com>
2015-10-28 20:06:12 -0400

    Adding blueprint/comments for work that needs to be done for the naive FirstRequest system

--- src/com/pmc1/system/FirstRequestProcessedSystem.java
+++ src/com/pmc1/system/FirstRequestProcessedSystem.java
@@ -1,5 +1,7 @@
 package com.pmc1.system;
 
+import com.pmc1.entity.Floor;
+
 /**
  * Very primitive elevator system that processes floor requests in the order that it receives input.
  * Elevators are given people based on the closest elevator about to pass the floor.
@@ -7,4 +9,14 @@ package com.pmc1.system;
  * Created by Paul on 10/28/2015.
  */
 public class FirstRequestProcessedSystem extends BaseElevatorSystem {
+
+    public void floorRequest(boolean goingUp) {
+        // first find an elevator that should pick up this request
+
+        // set that elevator's floorsOfInterest to be true for that specific floor
+    }
+
+    public void elevatorRequest(Floor floor) {
+        // process request in the order of request given because of naive implementation
+    }
 }
