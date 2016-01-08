cc569dcaa162b81cc3d91e2d6d6aad730b37a71c
paul <paulmchoi@gmail.com>
2016-01-08 15:04:07 -0500

    Adding person generator that generated people wiht start and destined floors. next step is to work on the environment that generates these numbers

--- src/com/pmc1/environment/PersonGenerator.java
+++ src/com/pmc1/environment/PersonGenerator.java
@@ -1,7 +1,30 @@
 package com.pmc1.environment;
 
+import java.lang.IndexOutOfBoundsException;
+
 /**
+ * Simple generator of people that gets start/destined floors from the supplied environment.
  * Created by Paul on 12/8/2015.
  */
 public class PersonGenerator {
+    private ArrayList<Floor> floorList;
+    private Environment environment;
+
+    public PersonGenerator(ArrayList<Floor> floorList, Environment environment) {
+        this.floorList = floorList;
+        this.environment = environment == null ? new RandomEnvironment() : environment;
+    }
+
+    public Person generatePerson() {
+        try {
+            Floor startFloor = floorList.get(environment.getStartFloor());
+            Floor desiredFloor = floorList.get(environment.getDestinedFloor());
+
+            Person person = new Person(desiredFloor);
+            startFloor.getPeopleOnFloor().add(person);
+        } catch (IndexOutOfBoundsException e) {
+            //TODO: log and count error
+            System.out.println("Generated person on floor " + startFloorNumber + " with desired floor " + desiredFloor);
+        }
+    }
 }
